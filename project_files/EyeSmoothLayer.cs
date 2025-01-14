de24e3b781434852d8a0c534690f59b57fb1d453
lunazera <cozykyra@gmail.com>
2025-01-13 22:04:55 -0500

    Update v1.1 tweaking smoothing functions
    - Added jitter removal
    - Added speed boost proportional to eye rotation size (a larger saccade will get a larger speed boost)
    - added eyeBlink blendshape threshold
    - Updated UI window with new settings controls.
    - Adjusted plugin logic so that new values are only written when a change is detected in the settings.

--- project_files/EyeSmoothLayer.cs
+++ project_files/EyeSmoothLayer.cs
@@ -12,14 +12,44 @@ namespace EyeSmoothLayer
         public static void setEyeSmoothLayerOnOff(float val) => EyeSmoothLayerActive = (val == 1f) ? true : false;
 
         public static Quaternion eyeBones;
+        public static Quaternion eyeBoneRotateTowards;
+
         // Eye bones: bone 21, 22
         public static int eyeLeft = 21;
         public static int eyeRight = 22;
 
         public static float speed = 10.0f;
+        public static float angleSpeed = 80f;
+        public static float jitter_removal = 0.01f;
+        public static float blendshapeBlinkThreshold = 50f;
+
+        public static void setJitterRemoval(float val)
+        {
+            jitter_removal = val;
+        }
+
+        public static void setAngleSpeed(float val)
+        {
+            angleSpeed = val;
+        }
+        public static void setBlendshapeBlinkThreshold(float val)
+        {
+            blendshapeBlinkThreshold = val;
+        }
+
         public static void setEyeSmoothSpeed(float val)
         {
+            // Inverted speed value, so that as user increases the "Smooth" setting which calls 'setEyeSmoothSpeed', this number goes from 360 (effectively no smoothing) to 0 (max smoothing)
+            // Clamped, so that val cannot be evaluated below 1
             speed = (val < 1f) ? 1000 : 1000 * (1 / val);
+        }
+
+        public static float setEyeSmoothAngleAdjustment(Quaternion qFrom, Quaternion qTo)
+        {
+            // Large rotation differences get a "boost" through this
+            float anglescaled = angleSpeed * Quaternion.Angle(qFrom, qTo)/15;
+
+            return anglescaled;
         }
     }
 
@@ -36,7 +66,11 @@ namespace EyeSmoothLayer
         public VNyanQuaternion RootRot;
 
         public Quaternion eyeBoneCurrent;
-        public float smoothedSpeed = 0f;
+        public float angleBetweenEyeBones;
+        public float angleSpeed;
+
+        public float blendshape_eyeBlinkLeft;
+        public float blendshape_eyeBlinkRight;
 
         // VNyan Get Methods, VNyan uses these to get the pose after doUpdate()
         VNyanVector3 IPoseLayer.getBonePosition(int i)
@@ -71,14 +105,26 @@ namespace EyeSmoothLayer
             RootPos = EyeSmoothFrame.RootPosition;
             RootRot = EyeSmoothFrame.RootRotation;
 
-            // get current Eye Bone rotations as Unity Quat
-
             eyeBoneCurrent = VNyanExtra.QuaternionMethods.convertQuaternionV2U(BoneRotations[EyeSmoothSettings.eyeRight]);
-            EyeSmoothSettings.eyeBones = Quaternion.RotateTowards(EyeSmoothSettings.eyeBones, eyeBoneCurrent, EyeSmoothSettings.speed * Time.deltaTime);
+            angleBetweenEyeBones = Quaternion.Angle(EyeSmoothSettings.eyeBones, eyeBoneCurrent);
+
+            if (angleBetweenEyeBones >= EyeSmoothSettings.jitter_removal)
+            {
+                blendshape_eyeBlinkLeft = VNyanInterface.VNyanInterface.VNyanAvatar.getBlendshapeInstant("eyeBlinkLeft")*100;
+                blendshape_eyeBlinkRight = VNyanInterface.VNyanInterface.VNyanAvatar.getBlendshapeInstant("eyeBlinkRight")*100;
+                if ((blendshape_eyeBlinkLeft < EyeSmoothSettings.blendshapeBlinkThreshold) || (blendshape_eyeBlinkRight < EyeSmoothSettings.blendshapeBlinkThreshold))
+                {
+                    EyeSmoothSettings.eyeBoneRotateTowards = eyeBoneCurrent;
+                }
+            }
+
+            angleSpeed = EyeSmoothSettings.setEyeSmoothAngleAdjustment(EyeSmoothSettings.eyeBones, EyeSmoothSettings.eyeBoneRotateTowards);
+            EyeSmoothSettings.eyeBones = Quaternion.RotateTowards(EyeSmoothSettings.eyeBones, EyeSmoothSettings.eyeBoneRotateTowards, (EyeSmoothSettings.speed + angleSpeed) * Time.deltaTime);
 
             // Write to VNyan BoneRotation
             BoneRotations[EyeSmoothSettings.eyeLeft] = VNyanExtra.QuaternionMethods.convertQuaternionU2V(EyeSmoothSettings.eyeBones);
             BoneRotations[EyeSmoothSettings.eyeRight] = VNyanExtra.QuaternionMethods.convertQuaternionU2V(EyeSmoothSettings.eyeBones);
+
         }
     }
 }
--- project_files/EyeSmoothingPlugin.cs
+++ project_files/EyeSmoothingPlugin.cs
@@ -10,9 +10,23 @@ namespace VNyanEyeSmoothing
     {
         private string paramNameEyeSmoothActive = "LZ_eyeSmoothActive";
         public float eyeSmoothActive = 1f;
+        private float eyeSmoothActive_new = 1f;
 
         private string paramNameSmoothSpeed = "LZ_eyeSmoothSpeed";
         public float smoothSpeed = 20f;
+        private float smoothSpeed_new = 20f;
+
+        private string paramNameJitterRemoval = "LZ_eyeSmoothJitter";
+        public float jitter_removal = 0.01f;
+        private float jitter_removal_new = 0.01f;
+
+        private string paramNameAngleSpeed = "LZ_eyeSmoothAngleSpeed";
+        public float angleSpeed = 80f;
+        private float angleSpeed_new = 80f;
+
+        private string paramNameEyeBlinkThreshold = "LZ_eyeSmoothBlinkThreshold";
+        public float eyeBlinkThreshold = 50f;
+        private float eyeBlinkThreshold_new = 50f;
 
         IPoseLayer EyeSmooth = new EyeSmoothLayer.EyeSmoothLayer();
 
@@ -23,9 +37,30 @@ namespace VNyanEyeSmoothing
             {
                 smoothSpeed = Convert.ToSingle(Sja_UICore.VNyanParameters[paramNameSmoothSpeed]);
             }
+            if (Sja_UICore.VNyanParameters.ContainsKey(paramNameJitterRemoval))
+            {
+                jitter_removal = Convert.ToSingle(Sja_UICore.VNyanParameters[paramNameJitterRemoval]);
+            }
+            if (Sja_UICore.VNyanParameters.ContainsKey(paramNameAngleSpeed))
+            {
+                angleSpeed = Convert.ToSingle(Sja_UICore.VNyanParameters[paramNameAngleSpeed]);
+            }
+            if (Sja_UICore.VNyanParameters.ContainsKey(paramNameEyeBlinkThreshold))
+            {
+                eyeBlinkThreshold = Convert.ToSingle(Sja_UICore.VNyanParameters[paramNameEyeBlinkThreshold]);
+            }
 
             VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(paramNameSmoothSpeed, smoothSpeed);
+            VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(paramNameJitterRemoval, jitter_removal);
+            VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(paramNameAngleSpeed, angleSpeed);
+            VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(paramNameEyeBlinkThreshold, eyeBlinkThreshold);
             VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(paramNameEyeSmoothActive, eyeSmoothActive);
+
+            EyeSmoothSettings.setEyeSmoothSpeed(smoothSpeed);
+            EyeSmoothSettings.setEyeSmoothLayerOnOff(eyeSmoothActive);
+            EyeSmoothSettings.setJitterRemoval(jitter_removal);
+            EyeSmoothSettings.setAngleSpeed(angleSpeed);
+            EyeSmoothSettings.setBlendshapeBlinkThreshold(eyeBlinkThreshold);
 
             VNyanInterface.VNyanInterface.VNyanAvatar.registerPoseLayer(EyeSmooth);
         }
@@ -33,12 +68,41 @@ namespace VNyanEyeSmoothing
         public void Update()
         {
             // Get current values from VNyan parameters
-            smoothSpeed = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameSmoothSpeed);
-            eyeSmoothActive = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameEyeSmoothActive);
+        
+            smoothSpeed_new = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameSmoothSpeed);
+            if (smoothSpeed_new != smoothSpeed)
+            {
+                smoothSpeed = smoothSpeed_new;
+                EyeSmoothSettings.setEyeSmoothSpeed(smoothSpeed);
+            }
 
-            // Set layer settings
-            EyeSmoothSettings.setEyeSmoothLayerOnOff(eyeSmoothActive);
-            EyeSmoothSettings.setEyeSmoothSpeed(smoothSpeed);
+            eyeSmoothActive_new = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameEyeSmoothActive);
+            if (eyeSmoothActive_new != eyeSmoothActive)
+            {
+                eyeSmoothActive = eyeSmoothActive_new;
+                EyeSmoothSettings.setEyeSmoothLayerOnOff(eyeSmoothActive);
+            }
+
+            jitter_removal_new = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameJitterRemoval);
+            if (jitter_removal_new != jitter_removal)
+            {   
+                jitter_removal = jitter_removal_new;
+                EyeSmoothSettings.setJitterRemoval(jitter_removal);
+            }
+
+            angleSpeed_new = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameAngleSpeed);
+            if (angleSpeed_new != angleSpeed)
+            {
+                angleSpeed = angleSpeed_new;
+                EyeSmoothSettings.setAngleSpeed(angleSpeed);
+            }
+
+            eyeBlinkThreshold_new = VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(paramNameEyeBlinkThreshold);
+            if (eyeBlinkThreshold_new != eyeBlinkThreshold)
+            {
+                eyeBlinkThreshold = eyeBlinkThreshold_new;
+                EyeSmoothSettings.setBlendshapeBlinkThreshold(eyeBlinkThreshold);
+            }
         }
     }
 }
